8fe8456a12263286704b0e9a2aac7f166b6fbfc5
Blancerx17 <fluchtenberg14@gmail.com>
2022-03-24 17:15:16 -0300

    desafio-02

---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+const Navbar = () => {
+    return (
+        <div className="navBar">
+            <ul className="nav-menu">
+                <li class="nav-item">
+                    <a href="index.html" className="nav-link">Inicio</a>
+                </li>
+                <li className="nav-item">
+                    <a href="pages/menu.html" className="nav-link">Hamburguesas</a>
+                </li>
+                <li className="nav-item">
+                    <a href="pages/menuEnsaladas.html" className="nav-link">Ensaladas</a>
+                </li>
+                <li className="nav-item">
+                    <a href="pages/menuAlcohol.html" className="nav-link">Alcohol</a>
+                </li>
+                <li className="nav-item">
+                    <a href="pages/nosotros.html" className="nav-link">Nosotros</a>
+                </li>
+                <li className="nav-item">
+                    <a href="pages/contacto.html" className="nav-link">Contacto</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar;
