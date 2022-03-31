70a0f7090a7593e3343a474ec1a2d5e40a8b5508
Blancerx17 <fluchtenberg14@gmail.com>
2022-03-31 14:17:05 -0300

    itemCount

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css';
-import ItemListContainer from "./components/ItemListContainer";
+import React from 'react';
+import ItemListContainer from './components/ItemListContainer';
 import Navbar from './components/NavBar';
+import  'bootstrap/dist/css/bootstrap.min.css';
+
 
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet"></link>
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="navBar">
       <Navbar></Navbar>
-      
+      <ItemListContainer></ItemListContainer>
     </div>
   );
 }
